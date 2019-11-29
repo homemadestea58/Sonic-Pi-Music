b3775cd94ccef6793a36d987761863c56d9fa8b8
Homemadestea58 <conor.collins@spadbb.catholic.edu.au>
2019-11-30 09:11:55 +1100

    Updates

--- Code/Project1.rb
+++ Code/Project1.rb
@@ -1,10 +1,10 @@
-#sample "C:/Users/conor.SEANACERLAPTOP/Desktop/Sonic-Pi/Samples/01 - Just Dance - Lady Gaga.wav"
+sample "C:/Users/conor.SEANACERLAPTOP/Desktop/Sonic-Pi/Samples/01 - Just Dance - Lady Gaga.wav", amp: 5
 
 live_loop :foo do
   use_synth :tb303
   use_random_seed 9
   7.times do
-    play (scale :e2, :minor_pentatonic).pick - 0, release: 0.1, amp: 3, cutoff: rrand(70,  100), amp:1
+    #play (scale :e2, :minor_pentatonic).pick - 0, release: 0.1, amp: 3, cutoff: rrand(70,  100), amp:1
     sleep 0.125
   end
   
@@ -13,11 +13,17 @@ end
 live_loop :beats, sync: :foo do
   use_random_seed 100
   8.times do
-    sample :loop_amen, 0, beat_stretch: 8, amp: 3, onset: pick
+    #sample :loop_amen, 0, beat_stretch: 8, amp: 3, onset: pick
     sleep 0.125
   end
   
 end
+
+live_loop :track do
+  play 70
+  sleep 1
+end
+
 
 
 
